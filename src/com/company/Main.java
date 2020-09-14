5b9c4b9713f3fc46d4c743e0b65b3be650960469
MonaEl-robaiy <71075004+MonaEl-robaiy@users.noreply.github.com>
2020-09-14 20:10:37 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 101; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i == 42) {
-                System.out.println("Answer to the Ultimate Question of Life, the Universe, and\n" +
-                        "Everything”");
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything”);
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
 
